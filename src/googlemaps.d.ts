c4bf7ce93bf381ee23937d234f2a1a95e196992a
Tahmidu1 <162470302+Tahmidu1@users.noreply.github.com>
2025-03-26 22:44:01 +0000

    Delete src directory

--- src/googlemaps.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace google.maps.places {
-    class Autocomplete {
-      constructor(inputField: HTMLInputElement, opts?: object);
-      getPlace(): google.maps.places.PlaceResult;
-      addListener(eventName: string, handler: Function): void;
-    }
-  }
-  
